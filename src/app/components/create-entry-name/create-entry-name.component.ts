e442629f3dc3188647d3415ecdc01c4ebc0f301b
Tanatip Boonloy <Tanatip Boonloy>
2016-03-06 21:30:14 +0700

    new route

--- src/app/components/create-entry-name/create-entry-name.component.ts
+++ src/app/components/create-entry-name/create-entry-name.component.ts
@@ -1,12 +1,21 @@
 /* beautify ignore:start */
 import {Component} from 'angular2/core';
+import {Api} from '../../services/api';
+import {AppHeadComponent} from '../app-head/app-head.component';
 /* beautify ignore:end */
 
 @Component({
     selector: 'create-entry-name',
     styles: [require('./create-entry-name.component.scss').toString()],
-    template: require('./create-entry-name.component.html')
+    template: require('./create-entry-name.component.html'),
+    directives: [AppHeadComponent]
 })
 export class CreateEntryNameComponent {
+	constructor(private api: Api){
 
+	}
+	
+	ngOnInit() {
+		console.log('Create Story');
+	}
 }
