10270abb07be2cf133510ba468edf882b258fc9a
Rapeapach Suwasri <kagaminelenkyun@gmail.com>
2016-03-06 21:04:13 +0700

    add view

--- src/app/index.ts
+++ src/app/index.ts
@@ -9,6 +9,7 @@ import {Api} from './services/api';         // ./services/api/index.ts
 import {EntriesService} from './services/entries-service/entries-service.service';
 import {Home} from './components/home';     // ./components/home/index.ts
 import {ErrorComponent} from './components/error/error.component';
+import {EntryViewComponent} from './components/entry-view/entry-view.component';
 //import {About} from "./components/about"; // About is an async route configured via @RouteConfig
 
 /*
@@ -26,6 +27,7 @@ import {ErrorComponent} from './components/error/error.component';
 @RouteConfig([
     {path: '/', component: Home, name: 'Home'},
     {path: '/page/:page', component: Home, name: 'Page'},
+    {path: '/entry/:id', component: EntryViewComponent, name: 'Entry'},
     // Async load a component using Webpack's require with es6-promise-loader
     {path: '/about', loader: () => require('./components/about')('About'), name: 'About' },
     {path: '/**', component: ErrorComponent, name: 'Error'}
